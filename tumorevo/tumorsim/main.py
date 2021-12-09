c0d4bae07c813db1e1f7f47e6d7e624061c5e287
pedrofale <pedro.ferreira@bsse.ethz.ch>
2021-12-09 15:27:32 +0100

    Refactor project

---
+++ tumorevo/tumorsim/main.py
@@ -0,0 +1,74 @@
+"""
+Simulate tumor growth under different spatial models. Inspired by Noble et al, 2019.
+"""
+from .cell import TumorCell
+from .modes import *
+
+import numpy as np
+import pandas as pd
+
+import click
+import os
+from pathlib import Path
+
+MODE_LIST = [
+	simulate_nonspatial,
+	simulate_invasion,
+	simulate_fission,
+	simulate_boundary, 
+]
+
+@click.command(help="Simulate tumor evolution under different spatial constraints.")
+@click.option(
+    "-m", 
+    "--mode", 
+    default=0, 
+    help="Spatial structure."
+)
+@click.option(
+    "-k",
+    "--carrying-capacity",
+    default=100,
+    help="Deme carrying capacity.",
+)
+@click.option(
+    "-g",
+    "--genes",
+    default=100,
+    help="Number of genes.",
+)
+@click.option(
+    "-s",
+    "--steps",
+    default=1000,
+    help="Number of steps in simulation.",
+)
+@click.option(
+   "-d",
+   "--division-rate",
+   default=0.1,
+   help="Divison rate."
+)
+@click.option(
+   "-o",
+   "--output-path",
+   default="./out",
+   help="Output directory"
+)
+def main(mode, carrying_capacity, genes, steps, division_rate, output_path):
+	tumor_cell = TumorCell(n_genes=genes, division_rate=division_rate)
+	env, traces = MODE_LIST[mode](steps, tumor_cell)
+	genotypes, _ = env.get_genotype_frequencies()
+	parents = env.genotypes_parents
+
+	print(f"Simulation in mode {mode} finished.")
+
+	Path(output_path).mkdir(parents=True, exist_ok=True)
+	pd.DataFrame([t['genotypes_counts'] for t in traces]).fillna(0).to_csv(os.path.join(output_path, 'trace_counts.csv'))
+	pd.DataFrame([parents]).to_csv(os.path.join(output_path, 'parents.csv'))
+	pd.DataFrame(genotypes).to_csv(os.path.join(output_path, 'genotypes.csv'))
+
+	print(f"Saved results to {output_path}.")
+	
+if __name__ == '__main__':
+	main()

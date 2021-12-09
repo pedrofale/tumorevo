365b4c170a939e02ceabb2281bfb52f6c3871f5d
pedrofale <pedro.ferreira@bsse.ethz.ch>
2021-12-09 22:49:46 +0100

    Fix modules in test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -4,9 +4,9 @@ import pytest
 from click.testing import CliRunner
 import os
 
-from tumorevo import main
-from tumorevo.modes import *
-from tumorevo.cell import TumorCell
+from tumorevo.tumorsim import main
+from tumorevo.tumorsim.modes import *
+from tumorevo.tumorsim.cell import TumorCell
 
 MODE_LIST = [
         simulate_nonspatial,
@@ -22,7 +22,7 @@ def test_simulation(mode):
 	env, traces = MODE_LIST[mode](1000, tumor_cell)
 
 	assert len(traces) == 1000
-	assert isinstance(env.get_genotype_frequencies(), np.ndarray)
+	assert isinstance(env.get_genotype_frequencies(), tuple)
 
 @pytest.mark.parametrize("mode", list(range(len(MODE_LIST))))
 def test_cli(mode):
@@ -30,9 +30,11 @@ def test_cli(mode):
 
 	# run program
 	with runner.isolated_filesystem():
-		result = runner.invoke(main, ["--mode", mode])
+		result = runner.invoke(main, ["--mode", mode, "-o", "./outs"])
 
 		# test output
 		assert result.exit_code == 0
-		assert os.path.isfile('adjacency.csv') 
+		assert os.path.isfile('./outs/parents.csv') 
+		assert os.path.isfile('./outs/trace_counts.csv')
+		assert os.path.isfile('./outs/genotypes.csv')
 
